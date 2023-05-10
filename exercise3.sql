6392f0e49781b52f95ad091d71076ef909c07534
Ubuntu <ubuntu@ip-172-31-25-129.ap-southeast-1.compute.internal>
2023-05-10 09:56:07 +0000

    exercise3.sql and exercise3.log

--- exercise3.sql
+++ exercise3.sql
@@ -1,68 +1,80 @@
+/*product table */
+DROP TABLE IF EXISTS products;
 CREATE TABLE IF NOT EXISTS products (
-id int,
-name VARCHAR(50),
-description TEXT,
-category_id int,
-branch_id int
-);
+	id int,
+	name VARCHAR(50),
+	description TEXT,
+	category_id int NOT NULL,
+	branch_id int NOT NULL
+	);
 
 
+	CREATE SEQUENCE products_id_seq
+	 as integer START 1 OWNED by products.id;
+
+	ALTER TABLE products
+	 ALTER COLUMN id SET DEFAULT nextval('products_id_seq');
+
+	ALTER TABLE products
+	 ADD CONSTRAINT pk_products
+	   PRIMARY KEY (id);
+
+
+	/*categories table */
+DROP TABLE IF EXISTS categories;
 CREATE TABLE IF NOT EXISTS categories (
-id int,
-name VARCHAR(50)
-);
+	id int,
+	name VARCHAR(50)
+	);
 
+	CREATE SEQUENCE categories_id_seq
+	 as integer START 1 OWNED by categories.id;
+
+	ALTER TABLE categories
+	 ALTER COLUMN id SET DEFAULT nextval('categories_id_seq');
+
+	ALTER TABLE categories
+	 ADD CONSTRAINT pk_categories
+	   PRIMARY KEY (id);
+
+
+	/*branches  table */
+DROP TABLE IF EXISTS branches;
 CREATE TABLE IF NOT EXISTS branches (
-id int,
-name VARCHAR(50)
-);
+	id int,
+	name VARCHAR(50)
+	);
 
 
-CREATE SEQUENCE categories_id_seq
- as integer START 1 OWNED by products.category_id;
+	CREATE SEQUENCE branches_id_seq
+	 as integer START 1 OWNED by branches.id;
 
-CREATE SEQUENCE branches_id_seq
- as integer START 1 OWNED by products.branch_id;
-
-ALTER TABLE products
- ALTER COLUMN category_id SET DEFAULT nextval('categories_id_seq');
-
-ALTER TABLE products
- ALTER COLUMN branch_id SET DEFAULT nextval('branches_id_seq');
-
-ALTER TABLE products
- ADD CONSTRAINT pk_products
-   PRIMARY KEY (id);
+	ALTER TABLE branches
+	 ALTER COLUMN id SET DEFAULT nextval('branches_id_seq');
 
 
-ALTER TABLE categories
- ADD CONSTRAINT pk_categories
-   PRIMARY KEY (id);
-
-ALTER TABLE branches
- ADD CONSTRAINT pk_branches
-   PRIMARY KEY (id);
+	ALTER TABLE branches
+	 ADD CONSTRAINT pk_branches
+	   PRIMARY KEY (id);
 
 
-ALTER TABLE products
-ADD CONSTRAINT fk_pk_categories
-FOREIGN KEY (category_id)
-REFERENCES categories (id);
+	ALTER TABLE products
+	ADD CONSTRAINT fk_pk_categories
+	FOREIGN KEY (category_id)
+	REFERENCES categories (id);
 
-ALTER TABLE products
-ADD CONSTRAINT fk_pk_branches
-FOREIGN KEY (branch_id)
-REFERENCES branches (id);
-
-
-INSERT INTO categories (id,name) VALUES (1,'fresh');
-INSERT INTO categories (id,name) VALUES (2,'frozen');
-
-INSERT INTO branches (id,name) VALUES (1,'SM');
-INSERT INTO branches (id,name) VALUES (2,'SNR');
-
-
-INSERT INTO products (id, name, description, category_id, branch_id ) values('1','SM_PRODUCTS','training exercise2',1,1);
+	ALTER TABLE products
+	ADD CONSTRAINT fk_pk_branches
+	FOREIGN KEY (branch_id)
+	REFERENCES branches (id);
 
 
 
+	INSERT INTO categories (name) VALUES ('fresh');
+	INSERT INTO categories (name) VALUES ('frozen');
+
+	INSERT INTO branches (name) VALUES ('SM');
+	INSERT INTO branches (name) VALUES ('SNR');
+
+
+	INSERT INTO products (name, description, category_id, branch_id ) values('SM_PRODUCTS','training exercise2',1,1);
